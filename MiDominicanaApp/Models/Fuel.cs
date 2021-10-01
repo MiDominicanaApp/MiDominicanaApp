392d279c8e51f199ac86f1d98fd3ced2382f66b0
crisog <ortega.cpp@gmail.com>
2021-10-01 00:28:15 -0400

    feat: add fuel model & fuel response model

---
+++ MiDominicanaApp/Models/Fuel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiDominicanaApp.Models
+{
+    public class Fuel
+    {
+        public string PremiumGasoline { get; set; }
+        public string RegularGasoline { get; set; }
+        public string OptimalDiesel { get; set; }
+        public string RegularDiesel { get; set; }
+        public string Kerosene { get; set; }
+        public string PetroleumLiquidGas { get; set; }
+        public string NaturalGas { get; set; }
+    }
+}
---
+++ MiDominicanaApp/Models/FuelResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace MiDominicanaApp.Models
+{
+    public class FuelResponse
+    {
+
+        [JsonPropertyName("gasolinaPremium")]
+        public string PremiumGasoline { get; set; }
+
+        [JsonPropertyName("gasolinaRegular")]
+        public string RegularGasoline { get; set; }
+
+        [JsonPropertyName("gasoilOptimo")]
+        public string OptimalDiesel { get; set; }
+
+        [JsonPropertyName("gasoilRegular")]
+        public string RegularDiesel { get; set; }
+
+        [JsonPropertyName("kerosene")]
+        public string Kerosene { get; set; }
+
+        [JsonPropertyName("gasLicuadoPetroleoGLP")]
+        public string PetroleumLiquidGas { get; set; }
+
+        [JsonPropertyName("gasNaturalVehicularGNV")]
+        public string NaturalGas { get; set; }
+    }
+
+}
